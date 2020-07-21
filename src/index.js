a77020d498f2347e131c02e094ce088b93310998
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-21 22:01:08 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let input = document.getElementById('new-task-description').value;
     let li = document.createElement('li');
     let liText = document.createTextNode(input);
+    let imp = document.getElementById("new-task-imp").value;
+    if (imp === "first"){
+      li.style.color = "red";
+    } else if (imp === "second") {
+      li.style.color = "orange";
+    } else if (imp === "third") {
+      li.style.color = "green";
+    }
     let xButton = document.createElement('button');
     xButton.appendChild(document.createTextNode("X"));
     xButton.setAttribute("data-description", `${input}`);

182ed31045802112b5d0a6804287909a126d3685
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-21 21:40:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+  const form = document.getElementById("create-task-form");
 
   document.querySelector('form').addEventListener('submit', function(e) {
     let input = document.getElementById('new-task-description').value;
-    document.getElementById('tasks').innerHTML += `<li>${input} </li>`;
+    let li = document.createElement('li');
+    let liText = document.createTextNode(input);
+    let xButton = document.createElement('button');
+    xButton.appendChild(document.createTextNode("X"));
+    xButton.setAttribute("data-description", `${input}`);
+    xButton.addEventListener('click', function(e){
+      this.parentElement.remove();
+    });
+
+    li.appendChild(liText);
+    li.appendChild(xButton);
+    document.getElementById('tasks').appendChild(li);
     e.preventDefault();
 
-    var form = document.getElementById("create-task-form");
+
     form.reset();
     });
 
